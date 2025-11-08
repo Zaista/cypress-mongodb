bf24361f95f1b35ef39064ee1dae1d2bd09848ee
Matthieu Kermagoret <matthieu@badakan.com>
2025-11-08 14:08:22 +0100

    chore: upgrade to Cypress 15

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'cypress';
-import { configurePlugin } from './dist';
+import { configurePlugin } from './src/index.ts';
 
 export default defineConfig({
   video: false,
   e2e: {
-    setupNodeEvents(on: any) {
+    setupNodeEvents(on, config) {
       configurePlugin(on);
+      return config;
     },
   },
 });
